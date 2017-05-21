8f065a7a637e73278c3d051f0b843c666dbd8197
WallfacerRZD <1048058400@qq.com>
2017-05-22 00:33:43 +0800

    test

---
+++ calculate/calculate.h
@@ -0,0 +1,32 @@
+
+// calculate.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CcalculateApp: 
+// 有关此类的实现，请参阅 calculate.cpp
+//
+
+class CcalculateApp : public CWinApp
+{
+public:
+	CcalculateApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CcalculateApp theApp;
---
+++ calculate/functions.h
@@ -0,0 +1,11 @@
+#ifndef  FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include<string>
+#include"BigData.h"
+
+std::string TransforToPostFix(const std::string &s);
+//int GetPostFixVal(const std::string &s);
+BigData GetPostFixVal(const string &data);
+
+#endif // ! FUNCTIONS_H
